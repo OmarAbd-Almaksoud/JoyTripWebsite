ebe7aa02587264df5ce1802fcb36e2ca65cf8c9c
Omar Mohamed <121135630+OmarAbd-Almaksoud@users.noreply.github.com>
2023-06-04 01:58:19 +0300

    del

--- src/app/Pipes/egpto-dollars.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EGPtoDollarsPipe } from './egpto-dollars.pipe';
-
-describe('EGPtoDollarsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EGPtoDollarsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
